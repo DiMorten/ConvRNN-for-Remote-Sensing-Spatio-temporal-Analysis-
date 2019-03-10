d5081886b5a712e4a6d62d48f3812b331b1f3b14
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2019-03-10 20:06:55 -0300

    Before getting back in main.py

--- src_seq2seq_ignorelabel/scripts/experiment_automation_lv2_cv.sh
+++ src_seq2seq_ignorelabel/scripts/experiment_automation_lv2_cv.sh
@@ -9,11 +9,11 @@ id='float32int8'
 
 
 
-dataset='cv'
+dataset='lm'
 #dataset='cv_seq1'
 
 # ==== EXTRACT PATCHES
-##. patches_extract.sh $dataset
+. patches_extract.sh $dataset
 # ===== USE MODEL
 
 #. experiment_automation.sh $id 'DenseNetTimeDistributed' $dataset 
@@ -22,6 +22,6 @@ dataset='cv'
 #. experiment_automation.sh $id 'ConvLSTM_seq2seq_bi_60x2' $dataset
 #. experiment_automation.sh $id 'FCN_ConvLSTM_seq2seq_bi_skip' $dataset
 
-. experiment_automation.sh $id 'ConvLSTM_seq2seq_bi' $dataset
+#. experiment_automation.sh $id 'ConvLSTM_seq2seq_bi' $dataset
 . experiment_automation.sh $id 'DenseNetTimeDistributed_128x2' $dataset  # gonna test balancing after replication
-. experiment_automation.sh $id 'ConvLSTM_seq2seq' $dataset
+#. experiment_automation.sh $id 'ConvLSTM_seq2seq' $dataset
